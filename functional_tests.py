1815f3c1de29b344a94746854da73496f7ace0f8
NumbMonkE <bhavinpatel47@gmail.com>
2021-10-07 15:02:20 +0530

    Functional test now checks if user can input to-do item.

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,27 +14,37 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-
-
-        #User wants to visit a web app with To-Do capabilities. User visits the homepage
+        # User wants to visit a web app with To-Do capabilities. User visits the homepage
         self.browser.get("http://localhost:8000")
 
-        #User sees the title and header with To-Do in it
+        # User sees the title and header with To-Do in it
         self.assertIn('To-Do', self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-
-        #User is prompted to enter a to-do list item
+        # User is prompted to enter a to-do list item
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertEqual("Enter a to-do item", inputbox.get_attribute('placeholder'))
 
         # User enters "1: get milk and eggs"
+        inputbox.send_keys("Get milk and eggs")
+
         # When user hits enter, the list is updated
+        inputbox.send_keys(Keys.ENTER)
+
         # There is still a box asking user to enter a to-do list item
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(rows.text == '1: Get milk and eggs' for row in rows))
+
+        self.fail("Finish the test!")
 
         # User enters "make cake batter"
         # User presses enter and the list updates again showing both items
         # User wants the site to remember her list of items.
         # A unique URL is generated for the user and there are some instructions explaining the same
         # User visits the given url and can see the previous list
+
 
 if __name__ == '__main__':
     # unittest.main(warnings='ignore')
