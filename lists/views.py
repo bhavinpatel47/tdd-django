76c7ce29c4a1edea6b90775b814242debcacb0e0
NumbMonkE <bhavinpatel47@gmail.com>
2021-10-07 13:09:17 +0530

    Tested for HTML and Title tags and values. Functional test still pending.

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 
-def home_page():
-    pass
+def home_page(request):
+    response = HttpResponse("<html><title>To-Do lists</title></html>")
+    return response
