9c6b40af019c611f8ec0068310f92f132a5984cb
NumbMonkE <bhavinpatel47@gmail.com>
2021-10-12 10:50:50 +0530

    Tested post request and adding items to database. Show in template.

--- functional_tests.py
+++ functional_tests.py
@@ -1,19 +1,25 @@
+import time
 import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import wait
 
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
+        self.wait = WebDriverWait(self.browser, 10)
         self.browser.implicitly_wait(3)
 
     def tearDown(self) -> None:
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
+        self.wait.until(EC.StaleElementReferenceException)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
@@ -37,21 +43,22 @@ class NewVisitorTest(unittest.TestCase):
         # When user hits enter, the list is updated
         inputbox.send_keys(Keys.ENTER)
 
-        self.browser.implicitly_wait(5)
         # There is still a box asking user to enter a to-do list item
+        time.sleep(1)
         self.check_for_row_in_list_table('1: Get milk and eggs')
 
         # self.assertTrue(any(row.text == '1: Get milk and eggs' for row in rows),
         #                 f'New to-do item did not appear in table.\nIt was {table.text}')
 
-
         # User enters "make cake batter"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Make cake batter")
+        time.sleep(1)
 
         # User presses enter and the list updates again showing both items
         inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(5)
+        time.sleep(1)
+
         self.check_for_row_in_list_table('1: Get milk and eggs')
         self.check_for_row_in_list_table('2: Make cake batter')
 
@@ -59,8 +66,6 @@ class NewVisitorTest(unittest.TestCase):
         # A unique URL is generated for the user and there are some instructions explaining the same
         # User visits the given url and can see the previous list
         self.fail("Finish the test!")
-
-
 
 
 if __name__ == '__main__':
--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from lists.models import Item
+
 
 def home_page(request: HttpRequest):
-    return render(request, "home.html", {'new_item_text': request.POST.get('item_text', '')})
+    # TODO: Support more than 1 list
+    # TODO: Display multiple items in the table
+
+    if request.method == "POST":
+        Item.objects.create(text=request.POST.get('item_text'))
+        return redirect("/")
+    items = Item.objects.all()
+    return render(request, "home.html", {'items': items})
