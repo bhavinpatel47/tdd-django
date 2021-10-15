4f39edaba2d9e3a1bb2187242b448e7a7c9df28a
NumbMonkE <bhavinpatel47@gmail.com>
2021-10-15 15:02:01 +0530

    Added css styling to templates and static_root in settings

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
 import time
 import unittest
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self) -> None:
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
