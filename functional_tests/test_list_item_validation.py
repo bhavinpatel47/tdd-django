90bfc47c7f8c340cddbfa6320a030e9e785dab75
NumbMonkE <bhavinpatel47@gmail.com>
2021-10-25 12:55:18 +0530

    Added FT for error message disappear on input. Refactored finding error message element.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,6 +7,9 @@ from .base import FunctionalTests
 
 class ItemValidationTest(FunctionalTests):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit an empty list item.
         self.browser.get(self.live_server_url)
@@ -51,6 +54,27 @@ class ItemValidationTest(FunctionalTests):
 
         # She sees a helpful error message about duplicate lists
         self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector('.has-error').text,
+            self.get_error_element().text,
             "You've already got this item in your list"
+        ))
+
+    def test_error_messages_are_cleared_on_input(self):
+        # Edith starts a list and causes a validation error
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Expecto Patronum!')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Expecto Patronum!')
+        self.get_item_input_box().send_keys('Expecto Patronum!')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertTrue(
+            self.get_error_element().is_displayed()
+        ))
+
+        # She starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('E')
+
+        # She is pleased to see that the error message disappears
+        self.wait_for(lambda: self.assertFalse(
+            self.get_error_element().is_displayed()
         ))
